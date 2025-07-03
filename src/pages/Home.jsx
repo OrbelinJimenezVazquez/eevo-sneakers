90926c6089472a70c66a34076c5f0e5be20dec98
OrbelinJimenezVazquez <200300671@ucaribe.edu.mc>
2025-07-03 09:58:37 -0700

    Agregar scripts de Font Awesome y mejorar la estructura de la página de inicio con secciones para nuevas llegadas y productos destacados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,28 @@ import products from '../data/products.json';
 import '/src/styles/Home.css';
 
 export default function Home() {
+  const featuredProducts = products.filter(prod => prod.featured);
+  const newArrivals = products.slice(0, 6); 
   return (
-    <div>
+    <div className="home-page">
       <h1>Bienvenido a Eevo Sneakers</h1>
-      {products.slice(0, 2).map(prod => (
-        <ProductCard key={prod.id} {...prod} />
-      ))}
+      <section className="new-arrivals">
+        <h2 className="section-title">Nuevas Llegadas</h2>
+        <div className="products-grid">
+          {newArrivals.map(product => (
+            <ProductCard key={product.id} {...product} />
+          ))}
+        </div>
+      </section>
+      <section className="featured-products">
+        <h2 className="section-title">Los Más Vendidos</h2>
+        <div className="products-grid">
+          {featuredProducts.map(product => (
+            <ProductCard key={product.id} {...product} />
+          ))}
+        </div>
+      </section>
+      
     </div>
   );
 }
