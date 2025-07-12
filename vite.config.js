a5f9c0b272e9f8472b65f0e141a2895a65adefd0
OrbelinJimenezVazquez <200300671@ucaribe.edu.mc>
2025-07-11 18:59:07 -0700

    fix: Corregir la ruta de importación del archivo JSON de productos en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import data from 'data/products/products.json'
+import data from './src/data/products/products.json'
 
 // https://vite.dev/config/
 export default defineConfig({
